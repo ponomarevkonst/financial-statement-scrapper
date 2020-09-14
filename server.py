bf405fe7331431261cd4079fa7fdd6ef07287bc6
Konstantin Ponomarev <konstantinponomarev@protonmail.com>
2020-09-14 15:33:07 +0300

    added api

---
+++ server.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+
+import uvicorn as uvicorn
+from fastapi import FastAPI, Query
+from nalog_scrapper import *
+
+app = FastAPI()
+
+
+@app.get("/organisations/")
+def read_items(inn: Optional[List[str]] = Query(['6704000505', '5321029508'])):
+    return main(inn)
+
+if __name__ == '__main__':
+    uvicorn.run("server:app", host="127.0.0.1", port=5000, log_level="info")
