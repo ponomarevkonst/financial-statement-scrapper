46775f10c18f7ea421653eed418c836fffb09115
Konstantin Ponomarev <konstantinponomarev@protonmail.com>
2020-09-15 00:26:44 +0300

    change of ports and some improvements of playbook

--- server.py
+++ server.py
@@ -12,4 +12,4 @@ def read_items(inn: Optional[List[str]] = Query(['6704000505', '5321029508'])):
     return main(inn)
 
 if __name__ == '__main__':
-    uvicorn.run("server:app", host="127.0.0.1", port=5000, log_level="info")
+    uvicorn.run("server:app", host="127.0.0.1", port=80, log_level="info")
