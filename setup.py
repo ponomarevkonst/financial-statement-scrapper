bf3c9bf1e97aa6b974e65094ada21388baf1344c
Konstantin Ponomarev <40800751+ponomarevkonst@users.noreply.github.com>
2020-10-02 03:25:43 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def get_requires():
+    path = 'nalog_scarapper/requirements.txt'
+    filepath = pkg_resources.resource_filename(__name__, path)
+    with open(filepath) as file:
+        packages = [name.rstrip() for name in file.readlines()]
+    return packages
+
+setup(
+    name='RASFS scrapper',
+    version='',
+    packages=['nalog_scrapper'],
+    url='https://github.com/ponomarevkonst/RASFS-scrapper',
+    license='MIT',
+    author='Konstantin Ponomarev',
+    author_email='ponomarevkonst@gmail.com',
+    description='Selenium based web scrapper for retrieving financial statements indexes from bo.nalog.ru.',
+    install_requires=get_requires()
+)
